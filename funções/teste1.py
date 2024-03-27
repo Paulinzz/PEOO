b775aa4a51651ebd31bc0be5fac8d8f88a06176f
João Paulo <paulofernandesalves30@gmail.com>
2024-03-27 09:48:58 -0300

    Update teste1.py

--- funções/teste1.py
+++ funções/teste1.py
@@ -3,11 +3,11 @@ def eh_par(n):
 
 def par_ou_impar(n):
     if eh_par(n):
-        return "par"
+        return "Par"
     else:
-        return "ímpar"
+        return "Ímpar"
     
-quantas = int(input("Deseja quantas vezes?"))
+quantas = int(input("Deseja quantas vezes? "))
 
 for i in range(0, quantas):
     print(par_ou_impar(int(input("Digite o número para ver se é par: "))))
